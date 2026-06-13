763905e1687a4921a839e37958c7611550319e0d
EshanZaman <eshanzaman6699@gmail.com>
2026-06-13 10:06:42 +0600

    Delegate api/logout.php to AuthController

--- api/logout.php
+++ api/logout.php
@@ -1,9 +1,4 @@
 <?php
-require_once '../config/auth.php';
-require_once '../config/db.php';
-header('Content-Type: application/json');
-if (isLoggedIn()) {
-    logActivity($conn, $_SESSION['user_id'], 'Logout', 'User logged out');
-}
-session_destroy();
-echo json_encode(['success' => true]);
+require_once __DIR__ . '/../bootstrap.php';
+$controller = new AuthController();
+$controller->logout();
